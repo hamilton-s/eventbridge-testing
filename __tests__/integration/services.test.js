79e25228c9371c26e6a71237ea40a6bd5f5f68c7
Sarah Hamilton <sarahh@theodo.co.uk>
2021-09-15 16:48:40 +0100

    Remove unused code

--- __tests__/integration/services.test.js
+++ __tests__/integration/services.test.js
@@ -3,19 +3,14 @@ import { sleep, filename } from "./utils";
 
 import { AWSClient, EventBridge } from "sls-test-tools";
 
-const lambda = new AWSClient.Lambda()
+const lambda = new AWSClient.Lambda();
 let eventBridge;
-const s3 = new AWSClient.S3()
 
 jest.setTimeout(50000); // eventual consistency can take time
 
 describe("Integration Testing Event Bridge", () => {
   beforeAll(async () => {
-    eventBridge = await EventBridge.build("event-bridge")
-  });
-
-  afterAll(async () => {
-    await eventBridge.destroy()
+    eventBridge = await EventBridge.build("event-bridge");
   });
 
   it("service 1 emits an event to the correct EventBus when triggered", async () => {
@@ -32,25 +27,29 @@ describe("Integration Testing Event Bridge", () => {
     };
     await lambda.invoke(params).promise();
 
-    const eventBridgeEvents = await eventBridge.getEvents()
+    const eventBridgeEvents = await eventBridge.getEvents();
     expect(eventBridgeEvents).toHaveEvent();
     expect(eventBridgeEvents).toHaveEventWithSource("order.created");
   });
 
   it("service 2 writes the correct data to S3 when correct event pushed to EventBridge", async () => {
     // inject event onto the event bus
-    await eventBridge
-      .publishEvent("order.created", "example", JSON.stringify({ filename: filename }));
+    await eventBridge.publishEvent(
+      "order.created",
+      "example",
+      JSON.stringify({ filename: filename })
+    );
 
     await sleep(5000); // wait 5 seconds to allow event to pass
 
-    const params = {
-      Bucket: "example-dev-thumbnails-bucket",
-      Key: filename,
-    };
-
     // Assert that file was added to the S3 bucket
-    const obj = await s3.getObject(params).promise();
-    expect(obj.ContentType).toBe("application/pdf");
+    await expect("example-dev-thumbnails-bucket").toExistAsS3Bucket();
+    await expect("example-dev-thumbnails-bucket").toHaveS3ObjectWithNameEqualTo(
+      filename
+    );
+    await expect({
+      bucketName: "example-dev-thumbnails-bucket",
+      objectName: filename,
+    }).toHaveContentTypeEqualTo("application/pdf");
   });
 });
